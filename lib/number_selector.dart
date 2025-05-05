8da35a4a7a7c0bbed30181e9d188896de920f6db
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-05-05 15:59:34 +0700

    menambahkan class nummber selector

---
+++ lib/number_selector.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NumberSelector extends StatelessWidget {
+  const NumberSelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      color: Colors.black54,
+      child: Row(
+    );
+  }
+}
