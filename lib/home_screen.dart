419a3d9b30088e1a7e332793e720c60f7038f909
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-05-05 15:41:13 +0700

    menbuata file grid sudoku di sudoku_grid.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,3 +2,19 @@ import 'package:flutter/material.dart';
 import '../widgets/sudoku_grid.dart';
 import '../widgets/number_selector.dart';
 
+class HomeScreen extends StatelessWidget {
+    const HomeScreen({super.Key});
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(titlr: const Text('SUDOKU')),
+            body: Column(
+                children: const[
+                    Expanded(child: SudokuGrid()),
+                    NumberSelector(),
+                ],
+            ),
+        );
+    }
+}
